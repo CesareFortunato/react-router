74f7d8e271b4443e81890d4e93d2b753b257beeb
CesareFortunato <cesare.fortunato89@gmail.com>
2026-01-28 16:09:05 +0100

    feat: useEffect + products.map + props

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,10 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Card from "../components/Card";
 
 function Products() {
 
+    const productsUrl = "https://fakestoreapi.com/products";
+    const [products, setProducts] = useState([]);
 
-    return(
-        <h1>products</h1>
+    useEffect(() => {
+        axios.get(productsUrl)
+            .then(res => { setProducts(res.data); })
+            .catch(err => {
+                console.log(err);
+            });
+    }, [])
+
+    return (
+        <>
+            {products.map((product) => {
+                return (
+                    <div key={product.id}>
+                        <Card
+                            name={product.title}
+                            desc={product.description}
+                            price={product.price}
+                        />
+                    </div>
+                )
+            })}
+        </>
     )
+
+
+
 }
 
 export default Products
