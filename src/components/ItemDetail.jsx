5df506bb6d1fee86be6d60755da3eea6c2d35192
CesareFortunato <cesare.fortunato89@gmail.com>
2026-01-29 15:50:35 +0100

    feat: itemDetail with :id

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,14 +1,29 @@
-
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function ItemDetail() {
 
+  const endPointItems = "https://fakestoreapi.com/products";
+  const { id } = useParams();
+  const [detailProducts, setDetailProducts] = useState([]);
+
+  useEffect(() => {
+        axios.get(`${endPointItems}/${id}`)
+            .then(res => { setDetailProducts(res.data); })
+            .catch(err => {
+                console.log(err);
+            });
+    }, [id])
+
   return (
+    <>
+      <h1>
+        sei nel detail di {id}
+      </h1>
 
-    <h1>
-        sei nel detail
-    </h1>
 
-
+    </>
   )
 }
 
